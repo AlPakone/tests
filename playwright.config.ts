7878b6fa94a515b599ba0c61548372ba8102473d
AlPakone <markovilikegames@gmail.com>
2023-04-04 20:17:22 +0200

    fixes and deprication of "headless"

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 0 : 0,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [["line"], ["allure-playwright"]],
   use: {
     trace: 'on',
